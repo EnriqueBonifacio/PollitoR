9eaa3f37aaae5d5a6b2c02f22e2530924b24bf1c
EnriqueBonifacio <enrique.boni99@gmail.com>
2025-05-04 17:34:37 -0500

    Primera parte de la clase

--- prueba.R
+++ prueba.R
@@ -1,2 +1,31 @@
 x = 1
 x
+class(x)
+
+y <- 'Hola soy Enrique'
+y 
+class(y)
+
+# Fechas de tiempo
+v <- "2025-05-04"
+class(v)
+v2 <- as.Date(v)
+v2
+class(v2)
+
+w <- "02/03/2025"
+as.Date(w, format = '%d/%m/%Y')
+
+#valores logicos
+t <- 4 < 2
+t 
+class(t)
+
+##vectores
+#hacer una lista de numeros del 1 al 5 
+h <- c(1,2,3,4,5)
+r <- c(TRUE, 24 , 'hola')
+is.vector(h)
+class(h)
+class(r)
+#tipos de datos
